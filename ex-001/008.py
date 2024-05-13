51e344f6e7832222359b2ae4b73187aed85bce12
Kauacomtil <kauapvp145@gmail.com>
2024-05-13 14:38:57 -0300

    Update 008.py

--- ex-001/008.py
+++ ex-001/008.py
@@ -1,1 +1,5 @@
 #Faça um algoritmo para ler três números e imprimir se estes podem ou não formar um triângulo.Observação – Para formar os lados de um triângulo cada um dos valores tem que ser menor que a soma dos outros dois.
+num1 = int(input("Insira o primeiro número: "))
+num2 = int(input("Insira o segundo número: "))
+num3= int(input("Insira o terceiro número: "))
+if 
